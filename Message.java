ed66dd794b86542bb7f26a69f894045fda0280c3
Jordan Wendel <63760355+jwendel78@users.noreply.github.com>
2020-09-14 19:41:26 -0400

    Add files via upload

---
+++ Message.java
@@ -0,0 +1,197 @@
+
+/**
+Program: A Message
+Author: Jordan Wendel
+*/
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+
+public class Message {
+
+    private Date dateTime;
+    private String message;
+    private String contentType;
+  
+    public Message()
+    { 
+        message = "Default message";
+    }
+
+    public Message(Date dateTime, String message, String contentType)
+    {
+        dateTime = dateTime;
+        message = message;
+        contentType = contentType;
+    }
+
+    public void PrintMessage()
+    { /* Function that prints the message */
+
+        System.out.println("\n" + "Here is the message: "+ message);
+        System.out.println("The message was sent at: " + dateTime);
+        System.out.println("This message was of type " + contentType + "\n");
+    }
+
+    // TEMPORARILY HERE IN CASE THEY ARE NEEDED EVENTUALLY
+    // Access to instance variables
+    public String getMessage() { return message; }
+    public Date getDate() { return dateTime; }
+    public String getType() { return contentType; }
+
+    // TEMPORARILY HERE IN CASE THEY ARE NEEDED EVENTUALLY
+    // Methods to change the instance data individually
+    public void setMessage(String m) { message = m;}
+    public void setDate(Date d) { dateTime = d; }
+    public void setType(String t) { contentType = t;}
+
+    public static String ReadFromFile(String fileName) throws IOException
+    { /* Function to read the message from a text file */
+        
+        // Function variables
+        String line = "";
+        String text = "";
+    
+        System.out.println("Reading information from text file...");
+
+        //Attempt to read the message from the file the user provided
+        try 
+        { // Read the file using the Scanner class
+            File file = new File(fileName);
+            BufferedReader fileReader = new BufferedReader(new FileReader(file));
+            while((line = fileReader.readLine()) != null) // Reads each individual line until null
+            { 
+                // Combines each line for the entire message
+                text += line + " ";
+            }
+            fileReader.close();
+        }
+        
+        catch (FileNotFoundException e) 
+        { // Error catching
+            System.out.println("An error occured.");
+            e.printStackTrace();
+        }
+        System.out.println("Successfully read message! ");
+
+        return text; 
+    }
+
+
+    public static void WriteToFile(Date date, String message, String type, String fileName) throws IOException
+    { /* Function to save the message to a text file */
+
+        File file = new File(fileName);
+        
+        // If an existing file of fileName has not been created, it will be created
+        if (!file.exists()) 
+        {
+            file.createNewFile();
+        }
+
+        // Attempt to write the message to a file 
+        try
+        {
+            FileWriter fw = new FileWriter(fileName);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write("Message details: " + message +"\n");
+            bw.write("Sent at: " + date + "\n");
+            bw.write("Type: " + type + "\n");
+            System.out.println("Successfully wrote to the file. ");
+            bw.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String [] args) throws IOException
+    { /* Main function */
+
+        // Creating the socket
+        Socket socket;
+        PrintStream p;
+        String hostname;
+        int port = 5521;
+
+        // Setting hostname
+        if (args.length > 0)
+        {
+            hostname = args[0];
+        }
+        else
+        {
+            hostname = "localhost";
+        }
+        
+        Scanner cin = new Scanner(System.in);
+        String fileName;
+
+        // Message type
+        //System.out.print("What is the message type? ");
+        //String type = cin.next();
+        String type = "text";
+        
+        // Name of the file we will be importing from
+        //System.out.print("What is the name of the file you are reading from? ");
+        //fileName = cin.next();
+        fileName = "message.txt";
+
+        // Inputs message content to file
+        String m = ReadFromFile(fileName);
+
+        // Date of the message at the time of sending
+        Date date = new Date();
+    
+        // Creating an instance of the Message class
+        Message newMessage = new Message();
+        newMessage.message = m;
+        newMessage.dateTime = date;
+        newMessage.contentType = type;
+
+        // Prints message details from the Message class
+        newMessage.PrintMessage();
+
+        // Name of the file we will be exporting to
+        //System.out.print("What is the name of the output file? ");
+        //fileName = cin.next();
+        //fileName = "message_output.txt";
+
+        // Outputs message contents to file
+        //WriteToFile(newMessage.dateTime, newMessage.message, newMessage.contentType, fileName);
+
+
+        // SENDING THE MESSAGE
+        try
+        {
+            // Creating the socket over specified port
+            socket = new Socket(hostname, port);
+            try 
+            {
+                while(true)
+                {
+                    // Open the print stream
+                    p = new PrintStream(socket.getOutputStream());
+                    
+                    // Send the message object as bytes over the socket
+                    p.println("You have mail: " + "\n" + newMessage.toString()); 
+                    socket.close(); 
+                }
+            }
+            catch (IOException e)
+            {
+                socket.close();
+                System.err.println(e);
+            }
+        }
+        catch (IOException e)
+        {
+            System.err.println(e);
+        }
+
+        cin.close();
+    }
+}

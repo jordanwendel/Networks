bf3bc7a6510a01e735208d898b5a9d1effaa4e07
Jordan Wendel <jwendel78@gmail.com>
2020-09-23 14:51:02 -0400

    Gets 200 OK. Updated headers to toString()
    Need to add GET and POST methods

--- HttpResponse.java
+++ HttpResponse.java
@@ -10,22 +10,33 @@ public class HttpResponse
     String data;
 
     HashMap<String, String> ResponseHeader = new HashMap<String, String>();
+
+    public HttpResponse(String res)
+    {// Constructor
+        //Parse into response
+        //Split new lines
+    }
     
     String getHeader(String header)
     { /** Return the value of the specified header */
         return ResponseHeader.get(header);
     }
 
-    String getData(String d) 
+    String getData() 
     { /** Return just the data from the response */
-        return ResponseHeader.get(d);
+        return ResponseHeader.get(data);
     }
 
+    public String toString() { /** Prints response formatted like we saw in class */
+        
+        String line = "";
+        for(Map.Entry<String, String> entry : ResponseHeader.entrySet())
+        {
+            line += entry.getKey() + ": " + entry.getValue();
+        }
+        line += "\n";
 
-    public String toString()
-    { /** Prints response formatted like we saw in class */
-        return "";
+        return line;
     }
 
 }
-
